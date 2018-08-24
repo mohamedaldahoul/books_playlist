7d8a8936a62db7a8abab83ca78d5e61d34e7a082
Moaldahoul <ma811forever@gmail.com>
2018-08-24 23:26:27 +0200

    connecting to mlab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://mohamed:HYF.cph3@ds225382.mlab.com:25382/play_list');
+mongoose.connection.once('open', ()=>{
+  console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
